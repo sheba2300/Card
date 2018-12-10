66c53a2f6d60575132f4e8c02eb35a1bf2d744b8
BT_1N3_29@bktech.local <BT_1N3_29@bktech.local>
2018-12-10 10:16:14 -0500

    Initial commit

---
+++ src/Card.java
@@ -0,0 +1,41 @@
+public class Card {
+    private String rank, suit;
+    private int pointVal;
+    public Card(String rank, String suit, int pointVal)
+    {
+            this.rank = rank;
+            this.suit = suit;
+            this.pointVal = pointVal;
+    }
+    public String getRank()
+    {
+        return this.rank;
+    }
+    public String getSuit()
+    {
+        return this.suit;
+    }
+    public int getPointVal()
+    {
+        return this.pointVal;
+    }
+    public void setRank(String rank)
+    {
+        this.rank = rank;
+    }
+    public void setSuit(String suit)
+    {
+        this.suit = suit;
+    }public void setPointVal(int pointVal)
+    {
+        this.pointVal = pointVal;
+    }
+    public boolean checkValue(Card other)
+    {
+        return(this.pointVal == other.pointVal);
+    }
+    public String toString()
+    {
+        return(this.rank + " of " + this.suit + " value: " + this.pointVal);
+    }
+}
---
+++ src/Deck.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Deck {
+  ArrayList<Card> undealt;
+    ArrayList<Card> dealt;
+    public Deck(String[] rankList, String[] suitList, int[] pointValList)
+    {
+        for(int a = 0; a<rankList.length;a++)
+        {
+            for(int b = 0; b<rankList.length;b++)
+            {
+                for(int c = 0; c<rankList.length;c++)
+                {
+                    Card newCard = new Card(rankList(a),)
+                    undealt.add()
+                }
+            }
+        }
+    }
+}
