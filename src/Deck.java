c3ca29fc06ad80d8f0562bd17fe16be7136c0dff
sheba sow <ssow8171@bths.edu>
2018-12-11 10:14:20 -0500

    not done

--- src/Deck.java
+++ src/Deck.java
@@ -1,20 +1,48 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Deck {
   ArrayList<Card> undealt;
     ArrayList<Card> dealt;
     public Deck(String[] rankList, String[] suitList, int[] pointValList)
     {
+        undealt = new ArrayList<Card>();
         for(int a = 0; a<rankList.length;a++)
         {
-            for(int b = 0; b<rankList.length;b++)
+            for(int b = 0; b<suitList.length;b++)
             {
-                for(int c = 0; c<rankList.length;c++)
+                for(int c = 0; c<pointValList.length;c++)
                 {
-                    Card newCard = new Card(rankList(a),)
-                    undealt.add()
+                    Card newCard = new Card(rankList[a],suitList[b],pointValList[c]);
+                    undealt.add(newCard);
                 }
             }
         }
     }
+    public boolean isEmpty()
+    {
+        if(undealt.size() == 0)
+            return true;
+        return true;
+    }
+    public int size()
+    {
+        return undealt.size();
+    }
+    public Card deal()
+    {
+        if(undealt.size() <0)
+            return null;
+        else
+        {
+            Card chosenCard = undealt.get(0);
+            undealt.remove(0);
+            return chosenCard;
+        }
+    }
+    public void shuffle()
+    {
+
+    }
+
 }
